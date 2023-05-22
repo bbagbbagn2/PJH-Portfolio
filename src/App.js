1ddbf9885b14390927f4687a70b4586e2145bac3
bbagbbagn2 <pyoungh999@naver.com>
2023-05-23 05:15:58 +0900

    removw(ProfilePage) ProfilePage.js 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import MainPage from './MainPage';
-import ProfilePage from './ProfilePage';
 import WorkPage from './WorkPage';
 import IconDBPage from './IconDBPage';
 import ContactPage from './ContactPage/Main';
@@ -10,7 +9,6 @@ function App() {
      <Router>
        <Routes>
          <Route path='/' element={<MainPage/>}/>
-         <Route path='/about' element={<ProfilePage/>}/>
          <Route path='/work' element={<WorkPage/>}/>
          <Route path='/icon-db' element={<IconDBPage/>}/>
          <Route path='/contact' element={<ContactPage/>}/>
