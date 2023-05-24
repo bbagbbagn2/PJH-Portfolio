61c325ba80dc9136db2c7a53b9239b78dacf3541
bbagbbagn2 <pyoungh999@naver.com>
2023-05-24 23:01:59 +0900

    remove(IconDBPage) IconDBPage 제거

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import MainPage from './MainPage';
-import IconDBPage from './IconDBPage';
 
 function App() {
   return (
      <Router>
        <Routes>
          <Route path='/' element={<MainPage/>}/>
-         <Route path='/icon-db' element={<IconDBPage/>}/>
        </Routes>
      </Router>
   );
