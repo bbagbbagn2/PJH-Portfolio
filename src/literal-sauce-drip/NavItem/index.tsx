752275ecef03f5a04fb3567538141bee1f7ca02e
bbagbbagn2 <pyoungh999@naver.com>
2023-12-13 17:38:43 +0900

    tounou image 추가

--- src/literal-sauce-drip/NavItem/index.tsx
+++ src/literal-sauce-drip/NavItem/index.tsx
@@ -2,55 +2,55 @@ import React, { MouseEvent, ReactNode } from 'react';
 import styled from 'styled-components';
 
 type NavItemProps = {
-  href: string;
-  clickFn?: (event: MouseEvent) => void;
-  isShelfEnv?: boolean;
-  workURL?: string;
-  resumeURL?: string;
-  external?: boolean;
-  children?: ReactNode;
+	href: string;
+	clickFn?: (event: MouseEvent) => void;
+	isShelfEnv?: boolean;
+	workURL?: string;
+	resumeURL?: string;
+	external?: boolean;
+	children?: ReactNode;
 }
 
 const NavItem: React.FC<NavItemProps> = ({
-  href,
-  clickFn,
-  isShelfEnv,
-  workURL,
-  resumeURL,
-  external,
-  children
+	href,
+	clickFn,
+	isShelfEnv,
+	workURL,
+	resumeURL,
+	external,
+	children
 }) => {
 
-  const handleClick = (event: MouseEvent) => {
-    if (href && href.charAt(0) === '#') {
-      event.preventDefault();
-    }
+	const handleClick = (event: MouseEvent) => {
+		if (href && href.charAt(0) === '#') {
+			event.preventDefault();
+		}
 
-    if (clickFn) {
-      clickFn(event);
-    }
+		if (clickFn) {
+			clickFn(event);
+		}
 
-    if (
-      isShelfEnv &&
-      window.location.pathname !== href &&
-      ![workURL, resumeURL].includes(href)
-    ) {
-      event.preventDefault();
-      window.location.href = href;
-    }
-  };
+		if (
+			isShelfEnv &&
+			window.location.pathname !== href &&
+			![workURL, resumeURL].includes(href)
+		) {
+			event.preventDefault();
+			window.location.href = href;
+		}
+	};
 
-  return (
-    <li>
-      <NavItemLink
-        href={href}
-        onClick={handleClick}
-        {...(external && { target: '_blank', rel: 'noreferrer noopener' })}
-      >
-        {children}
-      </NavItemLink>
-    </li>
-  );
+	return (
+		<li>
+			<NavItemLink
+				href={href}
+				onClick={handleClick}
+				{...(external && { target: '_blank', rel: 'noreferrer noopener' })}
+			>
+				{children}
+			</NavItemLink>
+		</li>
+	);
 };
 
 export default NavItem;
