06722cc7b79d54017e30af374124bb70879eaff4
bbagbbagn2 <pyoungh999@naver.com>
2024-01-04 02:30:24 +0900

    수정

--- src/pages/project/Icondb.tsx
+++ src/pages/project/Icondb.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Layout from './layout.tsx';
-import CustomSlider from './Slider.tsx';
-import PostContainer from './Paragraph.tsx';
+import Layout from './layout';
+import CustomSlider from './Slider';
+import PostContainer from './Paragraph';
 
 import screen1 from "./work-images/icondb/screen1.png";
 import screen2 from "./work-images/icondb/screen2.png";
