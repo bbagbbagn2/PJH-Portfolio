cb6f316c6f7eee703104b5913945e738e03bbb4f
bbagbbagn2 <pyoungh999@naver.com>
2024-01-03 23:59:07 +0900

    router 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import * as React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useParams } from 'react-router-dom';
 import MainPage from './pages/Main/Main';
 import DetailPage from './pages/Detail/Detail';
 import IcondbPage from './pages/IcondbPage';
 
+function ProjectRouter() {
+  const { subpath } = useParams();
+
+  return (
+    <Routes>
+      <Route index element={<DetailPage />} />
+      <Route path='icondb' element={<IcondbPage />} />
+    </Routes>
+  )
+}
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<MainPage />} />
-      <Route path='/project' element={<DetailPage />} />
-      <Route path='/project/icondb' element={<IcondbPage />} />
+      <Route path='/project/*' element={<ProjectRouter />} />
     </Routes>
   );
 }
