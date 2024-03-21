a05e9d54e0be3275b40b62018c8c52a19d44fe38
bbagbbagn2 <pyoungh999@naver.com>
2024-03-21 10:43:21 +0900

    스타일링 네이밍 변경

--- src/pages/Detail/ProjectContainer/ProjectBox/ProjectInfo.tsx
+++ src/pages/Detail/ProjectContainer/ProjectBox/ProjectInfo.tsx
@@ -8,41 +8,35 @@ type ProjectInfoProps = {
     siteURL?: string;
   }
 }
+
 export default function ProjectInfo({ project }: ProjectInfoProps) {
   return (
     <Container>
-      <ProjectInfoHeading>{project.name}</ProjectInfoHeading>
+      <Title>{project.name}</Title>
       {project.siteName && project.siteURL && (
-        <ProjectInfoLink
+        <Link
           target="_blank"
           rel="noopener noreferrer"
           aria-label={project.name + ' live demo.'}
           href={project.siteURL}
         >
           {project.siteName}
-        </ProjectInfoLink>
+        </Link>
       )}
     </Container>
   );
 }
 
-const List = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  gap: 1.2rem;
-  justify-content: space-between;
-`;
-
 const Container = styled.div`
   padding: 2em;
 `;
 
-const ProjectInfoHeading = styled.h5`
+const Title = styled.h5`
   margin: 0;
   font-size: 1.125em;
 `;
 
-const ProjectInfoLink = styled.a`
+const Link = styled.a`
   white-space: pre;
   color: #3c8fdd;
 `;
