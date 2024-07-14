613973792048f867bd9d0304f8a48fe8aa0ab4ee
bbagbbagn2 <pyoungh999@naver.com>
2024-07-14 19:10:26 +0900

    Notification logo.svg 파일 추가

--- src/pages/Detail/ProjectContainer/ProjectBox/ProjectLogo.tsx
+++ src/pages/Detail/ProjectContainer/ProjectBox/ProjectLogo.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { ReactComponent as IconDB } from '@_logos/icondb-logo.svg';
 import { ReactComponent as Drinkable } from '@_logos/drinkable-logo.svg';
 import { ReactComponent as Personal } from '@_logos/Pjh-Portfolio logo.svg';
+import { ReactComponent as Notification } from '@_logos/notification-logo.svg';
 
 type ProjectLogoProps = {
   project: {
@@ -13,12 +14,14 @@ type ProjectLogoProps = {
 const ICONDB_LOGO = <IconDB />;
 const DRINKABLE_LOGO = <Drinkable />;
 const PERSONAL_LOGO = <Personal />;
+const NOTIFICATION_LOGO = <Notification />;
 
 export default function ProjectLogo({ project }: ProjectLogoProps) {
   return (
     <Figure>
       {project.name === 'Icondb' && ICONDB_LOGO}
       {project.name === 'Drinkable' && DRINKABLE_LOGO}
+      {project.name === 'Notification' && NOTIFICATION_LOGO}
       {project.name === 'Personal Website' && PERSONAL_LOGO}
       <figcaption className="visuallyhidden">{project.name} logo.</figcaption>
     </Figure>
